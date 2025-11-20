c6cb839e38600858d62d514d7e942e840b71b0f5
naveh-elya <naveh.elya@post.runi.ac.il>
2025-11-20 21:21:01 +0200

    finsihed primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,24 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        System.out.println("Prime numbers up to "+ n +":");
+        int count = 0;
+        int num = 2;
+        while (count < n) {
+            if (isPrime(num)) {
+                System.out.println(num);
+                count++;
+            }
+            num++;
+        }
+        System.out.println("There are " + count + " primes between 2 and " + n + " (" + (count * 100 / n) + "% are primes)");
+    }
+    public static boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
